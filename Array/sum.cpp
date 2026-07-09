4e3f5337010925cba6e584a142b58f9eed5e4f9d
Md. Sakib Hossen <hassanmdshakib636@gmail.com>
2026-07-09 11:36:08 +0600

    Test after username change

--- Array/sum.cpp
+++ Array/sum.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main()
 {
-    cout << "Hello Github!";
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += i;
+    }
+    cout << sum << endl;
     return 0;
 }
